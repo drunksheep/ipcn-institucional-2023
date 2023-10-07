21f431753999ecf09cd43b55bddb3aed35a05bea
dvargas1 <daniel_v93@hotmail.com>
2023-10-06 21:57:51 -0300

    Footer adicionado

--- wp-content/themes/ipcn-institucional-2023/footer.php
+++ wp-content/themes/ipcn-institucional-2023/footer.php
@@ -1,6 +1,12 @@
 <?php wp_footer()?>
-    <footer class="flex flex-col bg-gray-500 pb-5">
-    <p> content</p>
+    <footer class="flex flex-col items-center gap-12 bg-gray-500 pb-5 p-10">
+        <div class="">
+            <h3 class="text-lg font-bold mb-2">Contatos</h3>
+            <p>Av. Mem de Sá, 208, Centro do Rio. RJ.</p>
+            <p>CEP: 20230-152 Tel.: (21) 97285-9639</p>
+            <p>Email: contato@ipcnbrasil.org</p>
+        </div>
+        <p class="text-sm">© 2023 IPCN Brasil. Todos os direitos reservados.</p>
     </footer>
 </body>
 </html>
