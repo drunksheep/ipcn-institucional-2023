03aeb82d50b66a0788b26e5bd47c114620bb459b
dvargas1 <daniel_v93@hotmail.com>
2023-10-06 21:00:55 -0300

    Header tortao

--- wp-content/themes/ipcn-institucional-2023/footer.php
+++ wp-content/themes/ipcn-institucional-2023/footer.php
@@ -1,4 +1,6 @@
 <?php wp_footer()?>
-    
+    <footer class="flex flex-col bg-gray-500 pb-5">
+    <p> content</p>
+    </footer>
 </body>
 </html>
--- wp-content/themes/ipcn-institucional-2023/header.php
+++ wp-content/themes/ipcn-institucional-2023/header.php
@@ -4,7 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?php wp_title()?> </title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <?php wp_head()?>
 </head>
 <body>
-    Bambucha
+    <header class="flex flex-col bg-gray-100 pb-5 ">
+    <a href="index.php" class="flex items-center mx-auto mt-4 mb-8"> <img src="wp-content\themes\ipcn-institucional-2023\img\logoIPCN.webp" alt="Logotipo IPCN" class="max-w-full h-auto"> </a>
+    <nav class="flex justify-between items-center bg-white mx-auto w-3/4 h-15 p-4 border border-gray-400 border-opacity-50 shadow">
+    <ul class="flex gap-10 justify-center w-full"> 
+        <li><a href="pagina1.php" class="font-sans font-bold hover:text-blue-500"> BOTAO 1</a></li>
+        <li><a href="pagina1.php" class="font-sans font-bold hover:text-blue-500"> BOTAO 1</a> </li>
+        <li><a href="pagina1.php" class="font-sans font-bold hover:text-blue-500"> BOTAO 1</a> </li>
+        <li><a href="pagina1.php" class="font-sans font-bold hover:text-blue-500"> BOTAO 1</a> </li>
+        <li><a href="pagina1.php" class="font-sans font-bold hover:text-blue-500"> BOTAO 1</a> </li>
+        <li><a href="pagina1.php" class="font-sans font-bold hover:text-blue-500"> BOTAO 1</a> </li>
+    </ul>
+    </nav>
+    </header>
